9f065e71f0e8d11a8ff22dd9c4daf729773605e8
sebastian.popa <sebastian.popa@softvision.com>
2023-08-02 12:27:45 +0300

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by sebastian.popa on 8/2/23.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
